089e668e899cb3ce7eae8d07c8e359437444e2b8
kv003 <kv003@DESKTOP-KO85RRN>
2022-11-03 17:44:18 +0900

    11월 3일 수업

--- p1103/src/p1103/J1103_08.java
+++ p1103/src/p1103/J1103_08.java
@@ -6,20 +6,19 @@ public class J1103_08 {
 
 	public static void main(String[] args) {
 		
-		// 6. 자신 번호 6개 숫자출력
-		// 7. 당첨번호개수 출력
-		
 		// 1. 45개 배열생성
 		Scanner scan = new Scanner(System.in);
-		int[] lotto_output = new int[45]; //화면출력 로또번호
+		String[] lotto_output = new String[45]; //화면출력 로또번호
 		int[] lotto = new int[45];        //당첨 로또번호
 		int[] input = new int[6];         //내가 입력한 로또번호
+		int[] l_no = new int[6];          //당첨된 번호
 		int count=0;                      //입력한 횟수
+		int num=0;                        //당첨개수
 		
 		// 2. 1-45까지 숫자 넣기
 		for(int i=0;i<lotto.length;i++) {
 			lotto[i] = i+1;
-			lotto_output[i] = i+1;
+			lotto_output[i] = i+1+"";
 		}//for
 		
 		// 3. 랜덤숫자 섞기
@@ -34,8 +33,27 @@ public class J1103_08 {
 		
 		// 4. 6개 번호넣기
 		while(count<6) {
+			System.out.println("        [ 로또 번호 ]");
+			int j=1;
+			for(int i=0;i<45;i++) {
+				if(j%5==0) {
+					System.out.printf("%s\t",lotto_output[i]);
+					System.out.println();
+				}else {
+					System.out.printf("%s\t",lotto_output[i]);
+				}//if
+				j++;
+			}//for
+			
+			
 			System.out.println("로또번호를 입력하세요.(1-45)>>");
 			input[count] = scan.nextInt();
+			for(int i=0;i<45;i++) {
+				if(lotto_output[i].equals(input[count]+"")) {
+					lotto_output[i]="♥";
+				}//if
+			}//for
+			
 			count++;
 		}//while
 		
@@ -43,16 +61,43 @@ public class J1103_08 {
 		System.out.println("     [ 로또 당첨확인 ]");
 		System.out.printf("로또 당첨번호 : ");
 		for(int i=0;i<6;i++) {
-			System.out.printf("%d ",lotto[i]);
+			System.out.printf("%d  ",lotto[i]);
 		}
 		System.out.println();
 		System.out.println("------------------------------");
 		
 		// 6. 자신 번호 6개 숫자출력
+		System.out.printf("나의 로또번호 : ");
+		for(int i=0;i<6;i++) {
+			System.out.printf("%d  ",input[i]);
+		}
+		System.out.println();
+		System.out.println("------------------------------");
 		
-
 		
+		// 7. 당첨번호개수 확인
+		for(int i=0;i<6;i++) {
+			for(int j=0;j<6;j++) {
+				if(lotto[i]==input[j]) {
+					l_no[num] = input[j]; //당첨된 번호를 l_no배열에 넣기
+					num++;
+					break;
+				}//if
+			}//for
+		}//for
 		
+		//8. 당첨번호개수, 번호출력
+		System.out.printf("당첨번호 개수 : %d \n",num);
+		System.out.println("-------------------------------");
+		System.out.printf("당첨번호 : ");
+		if(num==0) {
+			System.out.printf("당첨번호가 없습니다.");
+		}else {
+			for(int i=0;i<num;i++) {
+				System.out.printf("%d  ",l_no[i]);
+			}//for
+		}//if
+		System.out.println();
 		
 
 	}//main
---
+++ p1103/src/p1103/J1103_10.java
@@ -0,0 +1,10 @@
+package p1103;
+
+public class J1103_10 {
+
+	public static void main(String[] args) {
+		// 로또맞추기
+
+	}
+
+}
