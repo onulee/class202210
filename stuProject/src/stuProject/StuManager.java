8ca357cb1114f7adaf5fbaf21933baad7aa07b57
kv003 <kv003@DESKTOP-KO85RRN>
2022-12-08 10:05:50 +0900

    자바 성적관리시스템

---
+++ stuProject/src/stuProject/StuManager.java
@@ -0,0 +1,24 @@
+package stuProject;
+
+public class StuManager {
+
+	public static void main(String[] args) {
+		
+		//StuManager,StuCal,Student.... 클래스 생성해서 프로그램을 구성하시오.
+		
+		System.out.println("[ 성적처리프로그램 ]");
+		System.out.println("1. 성적입력");
+		System.out.println("2. 성적출력");
+		System.out.println("3. 성적수정");
+		System.out.println("4. 학생검색");
+		System.out.println("5. 등수처리");
+		System.out.println("6. 성적정렬선택");
+		System.out.println("7. 파일불러오기");
+		System.out.println("8. 파일저장하기");
+		System.out.println("0. 프로그램 종료");
+		System.out.println("-------------------------");
+		System.out.println("원하는 번호를 입력하세요.>>");
+
+	}
+
+}
